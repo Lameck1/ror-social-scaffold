8438441b1688d85fc8c7dc1be31a03d77189bb7e
Lameck1 <lameck72@gmail.com>
2021-03-08 11:44:41 +0300

    Model friendship

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,2 +1,14 @@
 class Friendship < ApplicationRecord
+  enum state: {
+    requesting: REQUESTING,
+    accepted: ACCEPTED,
+  }
+
+  validates :state, presence: true
+
+  belongs_to :user
+  belongs_to :friend, class_name: "User"
+
+  scope :requesting, -> { where(state: REQUESTING) }
+  scope :accepted, -> { where(state: ACCEPTED) }
 end
