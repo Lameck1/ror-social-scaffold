1186207a5a1b7dd38f21a027d867f9956548fd23
Lameck1 <lameck72@gmail.com>
2021-03-09 06:16:07 +0300

    Add associations for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,21 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  has_many :friendships, -> { where(state: Friendship::CONFIRMED) }
+  has_many :friends, through: :friendships
+
+  has_many :friend_requests_sent, lambda {
+                                    where(state: Friendship::REQUEST)
+                                  }, class_name: 'Friendship', foreign_key: 'user_id'
+  has_many :friend_requests_received, lambda {
+                                        where(state: Friendship::REQUEST)
+                                      }, class_name: 'Friendship', foreign_key: 'friend_id'
+  has_many :friend_requests, through: :friend_requests_received, source: :user
+
+  def friend?(user)
+    friendship = Friendship.find_by(user_id: user.id, friend_id: id, state: Friendship::CONFIRMED) ||
+                 Friendship.find_by(user_id: id, friend_id: user.id, state: Friendship::CONFIRMED)
+    true unless friendship.nil?
+  end
 end
