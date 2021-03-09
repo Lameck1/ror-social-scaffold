643228b365102d2f4e4e3c2fe61bb04af45c58f7
Lameck1 <lameck72@gmail.com>
2021-03-09 06:15:41 +0300

    Refactor Friendship model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,14 +1,9 @@
 class Friendship < ApplicationRecord
-  enum state: {
-    requesting: REQUESTING,
-    accepted: ACCEPTED,
-  }
-
-  validates :state, presence: true
-
   belongs_to :user
-  belongs_to :friend, class_name: "User"
+  belongs_to :friend, class_name: 'User', foreign_key: 'friend_id'
 
-  scope :requesting, -> { where(state: REQUESTING) }
-  scope :accepted, -> { where(state: ACCEPTED) }
+  STATES = [
+    REQUEST = 'request'.freeze,
+    CONFIRMED = 'confirmed'.freeze
+  ].freeze
 end
