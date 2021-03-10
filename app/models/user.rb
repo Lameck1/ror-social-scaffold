621a672fd498dfa53fecd95a8cd886beb9d1fdd3
Lameck1 <lameck72@gmail.com>
2021-03-10 10:51:48 +0300

    Refactor user model to get all friends

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,14 +16,28 @@ class User < ApplicationRecord
   has_many :friend_requests_sent, lambda {
                                     where(state: Friendship::REQUEST)
                                   }, class_name: 'Friendship', foreign_key: 'user_id'
+
   has_many :friend_requests_received, lambda {
                                         where(state: Friendship::REQUEST)
                                       }, class_name: 'Friendship', foreign_key: 'friend_id'
+
+  has_many :confirmed_requests, lambda { where(state: Friendship::CONFIRMED)}, class_name: 'Friendship', foreign_key: 'friend_id'
+  has_many :friendships_confirmed, through: :confirmed_requests, source: :user
+  has_many :requests_confirmed, lambda { where(state: Friendship::CONFIRMED)}, class_name: 'Friendship', foreign_key: 'user_id'
+  has_many :confirmed_friendships, through: :requests_confirmed, source: :friend
+
   has_many :friend_requests, through: :friend_requests_received, source: :user
 
   def friend?(user)
     friendship = Friendship.find_by(user_id: user.id, friend_id: id, state: Friendship::CONFIRMED) ||
                  Friendship.find_by(user_id: id, friend_id: user.id, state: Friendship::CONFIRMED)
     true unless friendship.nil?
+  end
+
+  def all_friends_ids
+    ids = []
+    fc = friendships_confirmed.ids
+    cf = confirmed_friendships.ids
+    fc + cf << id
   end
 end
